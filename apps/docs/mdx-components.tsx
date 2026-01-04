8df5c4ca477152de9256400f54b51e1c93f1ce05
Ashutosh <ashu.singh212@gmail.com>
2026-01-04 21:01:27 +0530

    chore: remove apps/docs - migrated to separate rigour-docs repo

--- apps/docs/mdx-components.tsx
+++
@@ -1,8 +0,0 @@
-import { useMDXComponents as getNextraMDXComponents } from 'nextra-theme-docs'
-import type { MDXComponents } from 'nextra/mdx-components'
-
-export function useMDXComponents(components: MDXComponents): MDXComponents {
-    return {
-        ...getNextraMDXComponents(components)
-    }
-}
--- apps/docs/next.config.mjs
+++
@@ -1,9 +0,0 @@
-import nextra from 'nextra'
-
-const withNextra = nextra({
-    // Nextra 4 specific options (empty for default)
-})
-
-export default withNextra({
-    reactStrictMode: true,
-})
